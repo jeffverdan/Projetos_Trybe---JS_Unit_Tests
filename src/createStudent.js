c0038dbe837f411b2633a25ae3a061ec09dc335b
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2021-11-13 18:06:42 -0300

    Req 5 - CreateStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (myName) => {
+  const ansObject = {
+    name: myName,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return ansObject;
+};
 
 module.exports = createStudent;
