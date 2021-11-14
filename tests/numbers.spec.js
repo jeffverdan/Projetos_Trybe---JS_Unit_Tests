1fe376617a920b595f8db825626cc92fd6423433
Petinger <lucaswinpetzinger@gmail.com>
2021-11-14 12:13:48 -0300

    requisitos 2  3 e 4

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,26 +16,32 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário' , () => {
+    expect(numbers([1, 2, 3, 4, 5])).toBeTruthy();
+    expect(numbers([1, 2, '3', 4, 5])).not.toBeTruthy();
+    expect(numbers([1, 'a', 3])).not.toBeTruthy();
+    expect(numbers([' '])).not.toBeTruthy();
+    expect(numbers([' '])).not.toBeTruthy();
+
     // fail("Teste vazio!"); 
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
     // Escreva um teste em que a função recebe [' '] e retorna false
   });
-  it('Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true', () => {
-    expect(numbers([1, 2, 3, 4, 5])).toBeTruthy();
+  // it('Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true', () => {
+    
    
-  });
-  it('Escreva um teste em que a função recebe [1, 2, "3", 4, 5] e retorna false', () => {
-    expect(numbers([1, 2, '3', 4, 5])).not.toBeTruthy();
+  // });
+  // it('Escreva um teste em que a função recebe [1, 2, "3", 4, 5] e retorna false', () => {
+   
     
-  });
-  it('Escreva um teste em que a função recebe [1, "a", 3] e retorna false', () => {
-    expect(numbers([1, 'a', 3])).not.toBeTruthy();
+  // });
+  // it('Escreva um teste em que a função recebe [1, "a", 3] e retorna false', () => {
+   
     
-  });
-  it('Escreva um teste em que a função recebe [" "] e retorna false', () => {
-    expect(numbers([' '])).not.toBeTruthy();
+  // });
+  // it('Escreva um teste em que a função recebe [" "] e retorna false', () => {
     
-  });
+  // });
+
 });
