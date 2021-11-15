04d93b045f897044d822342437f9b844c08dd8da
Marina Fischer <marinamirandafischer@gmail.com>
2021-11-15 19:41:02 -0300

    requisitos 5 e 6

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,14 +25,22 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle('3')).toBeUndefined();
     // Teste se circle retorna um objeto.
+    const retorno = circle(3)
+    const chavesRetorno = Object.keys(retorno);
+    expect(typeof(retorno)).toBe('object');
     // Teste se o objeto retornado tem 3 propriedades.
+    expect(chavesRetorno.length).toBe(3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle()).toBeUndefined();
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    expect(retorno.circumference).toBeCloseTo(18.84);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    expect(retorno.area).toBeCloseTo(28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    expect(retorno).toEqual({radius: 3, area: 28.259999999999998, circumference: 18.84})
   });
 });
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,13 +28,22 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    const retorno = productDetails('Alcool gel', 'Máscara');
+    const retorno2 = productDetails('luva', 'sabão')
+    expect(typeof retorno).toBe('object')
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(retorno.length).toBe(2)
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof retorno[0]).toBe('object');
+    expect(typeof retorno[1]).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(retorno).not.toEqual(productDetails(retorno2));
     // Teste se os dois productIds terminam com 123.
+    expect(retorno[0].details.productId).toMatch(/123/);
+    expect(retorno[1].details.productId).toMatch(/123/);
   });
 });
