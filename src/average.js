4a5c5a904e7b302aa527b5daa0cc81deb45ea38f
Caio Batista dos Santos <drcaiosan@gmail.com>
2021-11-16 14:26:36 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/average.js
+++ src/average.js
@@ -12,6 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  let sam = 0;
+
+  if (arr.length <= 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof arr[i] !== 'number') {
+      return undefined;
+    } else {
+      sam += arr[i];
+    }
+  }
+  let calculo = Math.round(sam / arr.length);
+  return calculo;
+  
+};
 
 module.exports = average;
--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,18 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if(typeof name !== 'string' ||typeof age !== 'number'){
+    return undefined;
+  }
+
+  return `Oi, meu nome é ${name}!
+  Tenho ${age} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+};
+
+// console.log(vqv('caio', 18))
+// console.log(vqv())
 
 module.exports = vqv;
