b1114f090aa59da9de4685b28f3f7506093982f5
fshinkae <felipeshinkae97@gmail.com>
2021-11-16 14:23:42 -0400

    Finalizado requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,24 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-teste
 
-const average = () => {};
+const average = (numbers) => {
+  let sum = 0;                                                        // Criado variável com inicio 0 para ser somado
+
+  if (numbers.length == 0) {                                          // If onde testa se o numero for 0 retornar undefined
+    return undefined
+  }
+
+  for (let index = 0; index < numbers.length; index += 1) {           // For para verificar cada elemento do array
+    if (typeof numbers[index] === "number") {                         // If para verificar se o tipo do dado é um numero
+      sum += numbers[index]                                           // Caso true adicionar o numero ao array sum criado anteriormente
+    } else {
+      return undefined                                                // Caso não retornar undefined 
+    }
+  }
+
+  return Math.round(sum / numbers.length)                            // Math.round (https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/round) aprendido função js para arredondamento da média
+
+};
 
 module.exports = average;
