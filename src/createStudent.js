1046576d98ddd7ff72ef2a3d8491c753b9e0ea58
Matheus Reusch <matheusreusch1@gmail.com>
2021-11-16 15:20:48 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let resultado = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return resultado;
+};
 
 module.exports = createStudent;
