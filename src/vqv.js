5126c8f50f328ff67dbbd537d708d0bff6cc108d
Elthon <elthon.rv@gmail.com>
2021-11-16 15:05:49 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = () => {};
-
+const vqv = (firstName, age) => {
+  if (typeof (firstName) !== 'string' || typeof (age) !== 'number') {
+    return undefined;
+  }
+  return `Oi, meu nome é ${firstName}!
+  Tenho ${age} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+};
 module.exports = vqv;
