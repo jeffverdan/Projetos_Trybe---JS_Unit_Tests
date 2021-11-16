3850348902a5524a04dc848640c57ce2842041d0
RiegerJulio <riegerproducoes@gmail.com>
2021-11-16 12:33:28 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const feedback = 'Eita pessoa boa!';
+  const student = {
+    name: nome,
+    feedback: () => feedback,
+  };
+
+  return student;
+};
 
 module.exports = createStudent;
