3df8da9caabb16f8253b8a6db9bd8637744ce7d0
Diego Dmitry <diegodmitry@hotmail.com>
2021-11-16 15:02:48 -0300

    the third task was done.

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
-
+const vqv = (string, num) => {
+  console.log(string, num);
+  if (string === undefined || num === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${string}!
+Tenho ${num} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
+vqv()
 module.exports = vqv;
