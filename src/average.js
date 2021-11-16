58e793cfc43d8c27ece100019677a5bd3f54d770
Elthon <elthon.rv@gmail.com>
2021-11-16 13:44:01 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // iniciando com tudo!
+const average = (array) => {
+  if (array.length < 1) {
+    return undefined;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    sum += array[i];
+  }
+  let avg = (sum / array.length);
+  avg = Math.round(avg);
+  return avg;
 };
 
 module.exports = average;
