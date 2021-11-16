4f9c1976a7a20e798c521d3c733bc6bbdedf5713
TiLourinho <lourinho.tiago@gmail.com>
2021-11-16 14:59:05 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const fb = () => 'Eita pessoa boa!';
+
+const createStudent = (str) => ({
+    name: str,
+    feedback: fb,
+});
 
 module.exports = createStudent;
