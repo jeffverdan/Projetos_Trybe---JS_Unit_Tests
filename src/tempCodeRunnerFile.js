a24068230db60b3666f0a1ff1cd97d22b81c781a
Vinicius Jacques <vinicius-jacques@hotmail.com>
2021-11-16 12:27:36 -0300

    Requisito 5

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,11 +1,8 @@
-const circle = (radius) => {
-  const PI = 3.14;
-
-  if (!radius || typeof radius !== 'number') { return undefined; }
-
-  return {
-    radius,
-    area: PI * radius * radius,
-    circumference: 2 * PI * radius,
-  }; 
-}; console.log(circle(2));
+const createStudent = (name) => {
+  const obj = {
+    name: name,
+    feedback:
+  };
+  
+};
+console.log(createStudent('Vinicius'));
