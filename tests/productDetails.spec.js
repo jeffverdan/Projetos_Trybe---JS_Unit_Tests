0feba4012503bdad0c2e0ef643d9e04cbd5ed63e
Rosalia Padoin Oliveira <rosalia@rosalia-VPCEH27FX>
2021-11-16 15:30:09 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -27,14 +27,43 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+  it('Teste se productDetails é uma função.', () => {
+    expect(typeof productDetails).toBe('function');
+  });
+  it('Teste se o retorno da função é um array.', () => {
+    expect(typeof productDetails()).toBe('object');
+  });
+  it('Teste se o array retornado pela função contém dois itens dentro.', () => {
+    expect(productDetails()).toEqual(
+      [
+        { name: undefined, details: { productId: 'undefined123' } },
+        { name: undefined, details: { productId: 'undefined123' } }
+      ]
+    );
+  });
+  it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+    const retorno = productDetails();
+    expect(typeof retorno[0]).toEqual('object');
+    expect(typeof retorno[1]).toEqual('object');
+  });
+  it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+    expect(productDetails('mascara', 'alcool')).toEqual(
+      [
+        { name: 'mascara', details: { productId: 'mascara123' } },
+        { name: 'alcool', details: { productId: 'alcool123' } }
+      ]
+    );
+    expect(productDetails('alcool', 'mascara')).toEqual(
+      [
+        { name: 'alcool', details: { productId: 'alcool123' } },
+        { name: 'mascara', details: { productId: 'mascara123' } }
+      ]
+    )
+  });
+  it('Teste se os dois productIds terminam com 123.', () => {
+    const retorno1 = productDetails()[0].details.productId;
+    const retorno2 = productDetails()[1].details.productId;
+    expect(retorno1.substr(-3, 3)).toEqual('123');
+    expect(retorno2.substr(-3, 3)).toEqual('123');
   });
 });
