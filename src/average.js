fdfa0f2c6914081e6f2b8c16e5da6c09c26c842d
Airam Toscano <airamlobato@gmail.com>
2021-11-16 14:31:58 -0400

    exercicio 1 finalizado

--- src/average.js
+++ src/average.js
@@ -12,10 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-
-
-  
+const average = (vetor) => {
+  if (vetor.length === 0) { // verifico se a função não está vazia. Se estiver retorna undefined.
+    return undefined;
+  }
+  let soma = 0;
+  let media = 0;
+  // logica de somatorio, vai somar todos meus elemetos presentes no vetor
+  for (let i = 0; i < vetor.length; i += 1) {
+    soma += vetor[i];
+    if (typeof vetor[i] === 'string') { // vou verificar se existe algum valor não numerico no meu vetor.
+      return undefined; // caso estiver me retorna um valor undefined
+    }
+  }
+  media = soma / vetor.length; // faço a media dos valores, pego a soma e divido pelo tamanho do meu vetor.
+  return Math.round(media); // retorna a media arredondada, ou seja, 4.5 vai pra 5 e 4.49 vai pra 4.
 };
 
 module.exports = average;
