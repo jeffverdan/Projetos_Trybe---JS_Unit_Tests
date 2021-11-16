95afec6463a7832ba657d3088cb196b0a82234bc
Degemar <degemarpereira@gmail.com>
2021-11-16 15:21:53 -0300

    Requisito 1 feito

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+
+  if (array.length === 0) {
+    return undefined;
+  } else {
+    for (let i = 0; i < array.length; i += 1) {
+      if (typeof array[i] !== 'number') {
+        return undefined;
+      } else {
+        soma += Math.round(array[i]);
+      }
+    }
+    return Math.round(soma/array.length);
+  }
+};
 
 module.exports = average;
