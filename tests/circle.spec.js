ba9bb4366d5f4895319fb46efb1c84c25b49ac20
Viviane Freitas <satierfviviane@gmail.com>
2021-11-16 14:43:33 -0300

    Inicia teste da funcao circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,14 +25,20 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle(typeof radius !== 'number')).toBeUndefined();
     // Teste se circle retorna um objeto.
+    expect(circle({})).toEqual({});
     // Teste se o objeto retornado tem 3 propriedades.
+    expect(Object.keys(circle())).toHaveLength(3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle(!radius)).toBeUndefined();
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
+
+
+// ToBeCloseTo: https://jestjs.io/pt-BR/docs/expect#tobeclosetonumber-numdigits
