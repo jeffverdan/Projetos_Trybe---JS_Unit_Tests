61860046a8fec46b77cd61c1efd4397b3bae0e95
Victor Roesner <victorroesner@hotmail.com>
2021-11-15 23:13:59 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (typeof name !== 'string' || typeof age !== 'number') {
+    return undefined;
+  } if (typeof name === 'string' && typeof age === 'number') {
+  const trybe = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return trybe;
+  }
+};
 
 module.exports = vqv;
