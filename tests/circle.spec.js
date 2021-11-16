e25c4d74d378176fea3329e1c8f1ab7811cc8445
Caio Batista dos Santos <drcaiosan@gmail.com>
2021-11-16 15:21:05 -0300

    inicio da realizacao do item 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const circle = require('../src/circle');
+const circle = require("../src/circle");
 
 /*
   Essa função recebe o raio de um círculo e retorna um objeto contendo suas informações (Raio, Área e Circunferência).
@@ -23,16 +23,35 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
+describe("4 - Implemente os casos de teste para a função `circle`", () => {
+  it("Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados", () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle("circulo")).toEqual("undefined");
     // Teste se circle retorna um objeto.
+    expect(typeof circle(2)).toEqual("object");
     // Teste se o objeto retornado tem 3 propriedades.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle()).toEqual("undefined");
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    const actual = circle(2);
+    const expected = { radius: 2, area: 12.56, circumference: 12.56 };
+    expect(actual).toEqual(expected);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    const actual = circle(3);
+    const expected = {
+      radius: 3,
+      area: 28.259999999999998,
+      circumference: 18.84,
+    };
+    expect(actual).toEqual(expected);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const actual = circle(3);
+    const expected = {
+      radius: 3,
+      area: 28.259999999999998,
+      circumference: 18.84,
+    };
+    expect(actual).toEqual(expected);
   });
 });
