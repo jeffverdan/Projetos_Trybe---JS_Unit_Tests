f1c437e335445b07de0254e5e8f2cc32e5c49536
Alana Correia <alanacorreia.ac@gmail.com>
2021-11-16 15:14:25 -0300

    realiza requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (array) => {
+  let sum = 0;
+  let result = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    sum += array[i];
+  }
+  result = Math.round(sum / array.length);
+  return result;
 };
 
 module.exports = average;
